f4a73256e20427e61349a8e32938ab673c7235d0
bxcar <malanchukdima@mail.ru>
2020-03-26 00:01:50 +0200

    some changes

--- app/js/common.js
+++ app/js/common.js
@@ -13,12 +13,73 @@ $(document).ready(function() {
         var py=y/ch*ih;
         /*console.log("click on "+this.tagName+" at pixel ("+px+","+py+") mouse pos ("+x+"," + y+ ") relative to boundingClientRect at ("+left+","+top+") client image size: "+cw+" x "+ch+" natural image size: "+iw+" x "+ih );*/
 
-        console.log(px+","+py);
+        // console.log(px+","+py);
 
-        if(py > 450 && py < 510) {
-            $(".level-5").css('display', 'block');
+        if(py > 460 && py <= 530) {
+            $(".main__img-overlays img").css({
+                "transition": "visibility 0.3s, opacity 0.3s linear",
+                "opacity": "0",
+                "visibility": "hidden"
+            });
+            $(".main__img-overlays img.level-5").css({
+                "transition": "visibility 0.3, opacity 0.3s linear",
+                "opacity": "1",
+                "visibility": "visible"
+            });
+            $("body").css('cursor', 'pointer');
+            $(".main__footer").css('z-index', '2');
+            // $(".main__image").css('background', '#203044').css('transition', 'background-color 0.3s linear');
+        } else if(py > 530 && py <= 600) {
+            $(".main__img-overlays img").css({
+                "transition": "visibility 0.3s, opacity 0.3s linear",
+                "opacity": "0",
+                "visibility": "hidden"
+            });
+            $(".main__img-overlays img.level-4").css({
+                "transition": "visibility 0.3s, opacity 0.3s linear",
+                "opacity": "1",
+                "visibility": "visible"
+            });
+            $("body").css('cursor', 'pointer');
+            $(".main__footer").css('z-index', '2');
+            // $(".main__image").css('background', '#203044').css('transition', 'background-color 0.3s linear');
+        } else if(py > 600 && py <= 670) {
+            $(".main__img-overlays img").css({
+                "transition": "visibility 0.3s, opacity 0.3s linear",
+                "opacity": "0",
+                "visibility": "hidden"
+            });
+            $(".main__img-overlays img.level-3").css({
+                "transition": "visibility 0.3s, opacity 0.3s linear",
+                "opacity": "1",
+                "visibility": "visible"
+            });
+            $("body").css('cursor', 'pointer');
+            $(".main__footer").css('z-index', '2');
+            // $(".main__image").css('background', '#203044').css('transition', 'background-color 0.3s linear');
+        } else if(py > 670 && py <= 740) {
+            $(".main__img-overlays img").css({
+                "transition": "visibility 0.3s, opacity 0.3s linear",
+                "opacity": "0",
+                "visibility": "hidden"
+            });
+            $(".main__img-overlays img.level-2").css({
+                "transition": "visibility 0.3s, opacity 0.3s linear",
+                "opacity": "1",
+                "visibility": "visible"
+            });
+            $("body").css('cursor', 'pointer');
+            $(".main__footer").css('z-index', '2');
+            // $(".main__image").css('background', '#203044').css('transition', 'background-color 0.3s linear');
         } else {
-            $(".level-5").css('display', 'none');
+            $(".main__img-overlays img").css({
+                "transition": "visibility 0.3s, opacity 0.3s linear",
+                "opacity": "0",
+                "visibility": "hidden"
+            });
+            $("body").css('cursor', 'default');
+            $(".main__footer").css('z-index', '4');
+            // $(".main__image").css('background', '#4a72a5').css('transition', 'background-color 0.3s linear');
         }
     });
 });
